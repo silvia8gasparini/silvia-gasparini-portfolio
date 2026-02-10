71e6085ee470a446917b088bf1531da06f747db9
Silvia <silvia8gasparini@gmail.com>
2026-02-10 19:07:36 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
       fontSize: {
       '5xl-custom': ['1.4rem', { lineHeight: '1.1' }],
       '5xl-ricustom': ['1.2rem', { lineHeight: '1.1' }],
-      '5xl-tomtom': ['2.4rem', { lineHeight: '1.1' }],
-      '5xl-tomcus': ['2.7rem', { lineHeight: '1.1' }],
+      '5xl-tomtom': ['2.6rem', { lineHeight: '1.1' }],
+      '5xl-tomcus': ['2.9rem', { lineHeight: '1.1' }],
       '4xl-custom': ['2.6rem', { lineHeight: '1.1' }],
       },  
       backgroundImage: {
